5aac5fac8f0ee6e7171d3cc65e0389f208b34979
long.phanhoang <long.phahoang@teragroup.io>
2024-03-17 10:25:21 +0700

    Edit logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,10 +19,10 @@
 
     if (window.pageYOffset > sticky) {
       header_navbar.classList.add("sticky");
-      logo.src = "assets/images/logo/logo-kimlong-black.svg";
+      logo.src = "assets/images/logo/logo-black.svg";
     } else {
       header_navbar.classList.remove("sticky");
-      logo.src = "assets/images/logo/logo-kimlong-white.svg";
+      logo.src = "assets/images/logo/logo-white.svg";
     }
 
     // show or hide the back-top-top button
